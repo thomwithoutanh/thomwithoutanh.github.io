d374523b64d6cf4e5ac76ec3c0ad93accd054017
Tom Walker <tpgwalker@gmail.com>
2021-02-02 10:53:40 +0000

    activate dark mode

--- _site/assets/js/search-worker-9.0.5.js
+++ _site/assets/js/search-worker-9.0.5.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2021-01-31T22:54:52+00:00';
+const INDEX_KEY = 'index--2021-01-31T22:56:50+00:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
